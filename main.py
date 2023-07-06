51671958bc5903d4dd843f81984ba53fa9ae2a1f
kirubalinee <138742632+kirubalinee@users.noreply.github.com>
2023-07-06 11:20:36 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+def main():
+    """Start guessing genre music between hip hop,jozz,rock,disco,classical and kpop."""
+    print("Guess the genre music between hip hop,jozz,rock,disco,classical and kpop")
+    
+    music = ("hip hop" , "jozz" , "rock" , "disco", "classical" , "kpop")
+    
+    x = random.choice(music)
+    guess = None
+    
+    while x !=guess:
+        
+        guess = str(input("pick a genre music:"))
+        
+        if x == guess:
+            print("You got it!!.")
+        else:
+            print("You can do it try agian.")
+            
+main()
